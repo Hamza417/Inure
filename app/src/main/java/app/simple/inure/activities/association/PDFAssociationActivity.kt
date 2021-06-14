9705ae6c0625919cb678d2ffe547cc2c1c6f521f
Hamza417 <hamzarizwan243@gmail.com>
2021-06-14 09:28:38 +0530

    Initial Build
      - Added PDF file association for viewing PDF files

---
+++ app/src/main/java/app/simple/inure/activities/association/PDFAssociationActivity.kt
@@ -0,0 +1,67 @@
+package app.simple.inure.activities.association
+
+import android.os.Bundle
+import android.view.animation.DecelerateInterpolator
+import android.widget.FrameLayout
+import androidx.core.view.doOnLayout
+import androidx.documentfile.provider.DocumentFile
+import app.simple.inure.R
+import app.simple.inure.decorations.padding.PaddingAwareLinearLayout
+import app.simple.inure.decorations.views.TypeFaceTextView
+import app.simple.inure.dialogs.miscellaneous.ErrorPopup
+import app.simple.inure.extension.activities.BaseActivity
+import com.github.barteksc.pdfviewer.PDFView
+import com.github.barteksc.pdfviewer.util.FitPolicy
+
+class PDFAssociationActivity : BaseActivity() {
+
+    private lateinit var pdfView: PDFView
+    private lateinit var name: TypeFaceTextView
+    private lateinit var pdfContainer: FrameLayout
+    private lateinit var header: PaddingAwareLinearLayout
+
+    private var isFullScreen = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pdf_viewer)
+
+        pdfView = findViewById(R.id.pdf_viewer)
+        name = findViewById(R.id.pdf_name)
+        pdfContainer = findViewById(R.id.pdf_container)
+        header = findViewById(R.id.header_pdf)
+
+        name.text = DocumentFile.fromSingleUri(this, intent!!.data!!)!!.name
+
+        header.doOnLayout {
+            pdfContainer.apply {
+                setPadding(paddingLeft, header.height + paddingTop, paddingRight, paddingBottom)
+            }
+
+            pdfView.useBestQuality(true)
+            pdfView.fromUri(intent!!.data!!)
+                    .defaultPage(0)
+                    .enableAnnotationRendering(true)
+                    .swipeHorizontal(false)
+                    .spacing(25)
+                    .fitEachPage(false)
+                    .onError {
+                        ErrorPopup.newInstance(it.message!!)
+                                .show(supportFragmentManager, "error")
+                    }
+                    .enableAntialiasing(true)
+                    .pageFitPolicy(FitPolicy.WIDTH)
+                    .onTap {
+                        if (isFullScreen) {
+                            header.animate().translationY(header.height.toFloat() * -1).setInterpolator(DecelerateInterpolator()).start()
+                            isFullScreen = false
+                        } else {
+                            header.animate().translationY(0F).setInterpolator(DecelerateInterpolator()).start()
+                            isFullScreen = true
+                        }
+                        true
+                    }
+                    .load()
+        }
+    }
+}
