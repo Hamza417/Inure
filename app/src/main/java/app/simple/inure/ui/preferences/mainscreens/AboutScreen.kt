2c663116da22d48fb0993d14cece6e9bff727626
Hamza Rizwan <hamzarizwan243@gmail.com>
2021-12-22 21:14:11 +0530

    Initial Build
      - Updated About panel design and elements

--- app/src/main/java/app/simple/inure/ui/preferences/mainscreens/AboutScreen.kt
+++ app/src/main/java/app/simple/inure/ui/preferences/mainscreens/AboutScreen.kt
@@ -1,16 +1,23 @@
 package app.simple.inure.ui.preferences.mainscreens
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import app.simple.inure.R
+import app.simple.inure.decorations.ripple.DynamicRippleRelativeLayout
 import app.simple.inure.extension.fragments.ScopedFragment
 
 class AboutScreen : ScopedFragment() {
 
+    private lateinit var github: DynamicRippleRelativeLayout
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        github = view.findViewById(R.id.about_github)
 
         return view
     }
@@ -19,6 +26,10 @@ class AboutScreen : ScopedFragment() {
         super.onViewCreated(view, savedInstanceState)
         startPostponedEnterTransition()
 
+        github.setOnClickListener {
+            val uri: Uri = Uri.parse("https://github.com/Hamza417/Inure")
+            startActivity(Intent(Intent.ACTION_VIEW, uri))
+        }
     }
 
     companion object {
