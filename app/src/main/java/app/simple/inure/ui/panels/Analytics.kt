d364ad72a3364bf63da274f430551f69b060e2da
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-03-11 12:34:02 +0530

    analytics_rev_05
      - Fixed solid center color for Pie charts
      - Fixed legend layout not padded properly

--- app/src/main/java/app/simple/inure/ui/panels/Analytics.kt
+++ app/src/main/java/app/simple/inure/ui/panels/Analytics.kt
@@ -15,6 +15,8 @@ import app.simple.inure.dialogs.menus.AnalyticsMenu
 import app.simple.inure.extension.fragments.ScopedFragment
 import app.simple.inure.preferences.AnalyticsPreferences
 import app.simple.inure.preferences.AppearancePreferences
+import app.simple.inure.themes.interfaces.ThemeChangedListener
+import app.simple.inure.themes.manager.Theme
 import app.simple.inure.themes.manager.ThemeManager
 import app.simple.inure.util.FragmentHelper
 import app.simple.inure.util.TypeFace
@@ -25,13 +27,15 @@ import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.utils.ColorTemplate
 
-class Analytics : ScopedFragment() {
+class Analytics : ScopedFragment(), ThemeChangedListener {
 
     private lateinit var settings: DynamicRippleImageButton
     private lateinit var search: DynamicRippleImageButton
     private lateinit var minimumOsPie: PieChart
     private lateinit var targetOsPie: PieChart
+    private lateinit var installLocationPie: PieChart
     private lateinit var minSdkHeading: TypeFaceTextView
 
     private val analyticsViewModel: AnalyticsViewModel by viewModels()
@@ -45,6 +49,7 @@ class Analytics : ScopedFragment() {
         search = view.findViewById(R.id.search_button)
         minimumOsPie = view.findViewById(R.id.minimum_os_pie)
         targetOsPie = view.findViewById(R.id.target_os_pie)
+        installLocationPie = view.findViewById(R.id.install_location_pie)
         minSdkHeading = view.findViewById(R.id.min_sdk_heading)
 
         if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.N) {
@@ -52,7 +57,9 @@ class Analytics : ScopedFragment() {
             minSdkHeading.gone()
         }
 
+        ThemeManager.addListener(this)
         startPostponedEnterTransition()
+
         return view
     }
 
@@ -84,6 +91,7 @@ class Analytics : ScopedFragment() {
                 }
 
                 holeRadius = 50F
+                setHoleColor(Color.TRANSPARENT)
                 setUsePercentValues(false)
                 dragDecelerationFrictionCoef = 0.95f
                 isHighlightPerTapEnabled = true
@@ -93,6 +101,14 @@ class Analytics : ScopedFragment() {
                 animateXY(1000, 500, Easing.EaseOutCubic)
             }
 
+            /**
+             * It's workaround for the bug that messes up the layout
+             * of the legend data and solved by call
+             * [PieChart.notifyDataSetChanged] two times.
+             *
+             * TODO - Find a solution
+             */
+            minimumOsPie.notifyDataSetChanged()
             minimumOsPie.notifyDataSetChanged()
             minimumOsPie.invalidate()
         }
@@ -122,17 +138,65 @@ class Analytics : ScopedFragment() {
                 }
 
                 holeRadius = 50F
+                setHoleColor(Color.TRANSPARENT)
                 setUsePercentValues(false)
                 dragDecelerationFrictionCoef = 0.95f
                 isHighlightPerTapEnabled = true
                 description.isEnabled = false
                 setExtraOffsets(chartOffset, chartOffset, chartOffset, chartOffset)
                 setDrawCenterText(false)
-                animateXY(1000, 500, Easing.EaseOutCubic)
+
+                /**
+                 * Won't be visible so we can save some rendering strength here
+                 * Let the only above one animate
+                 */
+                // animateXY(1000, 500, Easing.EaseOutCubic)
             }
 
             targetOsPie.notifyDataSetChanged()
+            targetOsPie.notifyDataSetChanged()
             targetOsPie.invalidate()
+        }
+
+        analyticsViewModel.getInstallLocationData().observe(viewLifecycleOwner) {
+            installLocationPie.apply {
+                PieDataSet(it.first, "").apply {
+                    data = PieData(this)
+                    colors = ColorTemplate.PASTEL_COLORS.toMutableList()
+                    valueTextColor = Color.TRANSPARENT
+                    setEntryLabelColor(Color.TRANSPARENT)
+                }
+
+                legend.apply {
+                    isEnabled = true
+                    formSize = 10F
+                    formToTextSpace = 5F
+                    form = Legend.LegendForm.DEFAULT
+                    textColor = ThemeManager.theme.textViewTheme.secondaryTextColor
+                    this.xEntrySpace = 20F
+                    this.yEntrySpace = 5F
+                    this.typeface = TypeFace.getTypeFace(AppearancePreferences.getAppFont(), TypeFace.TypefaceStyle.MEDIUM.style, requireContext())
+                    isWordWrapEnabled = true
+                    orientation = Legend.LegendOrientation.HORIZONTAL
+                    verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+                    horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+                }
+
+                holeRadius = 50F
+                setHoleColor(Color.TRANSPARENT)
+                setUsePercentValues(false)
+                dragDecelerationFrictionCoef = 0.95f
+                isHighlightPerTapEnabled = true
+                description.isEnabled = false
+                setExtraOffsets(chartOffset, chartOffset, chartOffset, chartOffset)
+                setDrawCenterText(false)
+
+                // animateXY(1000, 500, Easing.EaseOutCubic)
+            }
+
+            installLocationPie.notifyDataSetChanged()
+            installLocationPie.notifyDataSetChanged()
+            installLocationPie.invalidate()
         }
 
         settings.setOnClickListener {
@@ -154,6 +218,29 @@ class Analytics : ScopedFragment() {
                 analyticsViewModel.refresh()
             }
         }
+    }
+
+    override fun onThemeChanged(theme: Theme?) {
+        super.onThemeChanged(theme)
+        minimumOsPie.apply {
+            legend.textColor = ThemeManager.theme.textViewTheme.secondaryTextColor
+            invalidate()
+        }
+
+        targetOsPie.apply {
+            legend.textColor = ThemeManager.theme.textViewTheme.secondaryTextColor
+            invalidate()
+        }
+
+        installLocationPie.apply {
+            legend.textColor = ThemeManager.theme.textViewTheme.secondaryTextColor
+            invalidate()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ThemeManager.removeListener(this)
     }
 
     companion object {
--- app/src/main/java/app/simple/inure/viewmodels/panels/AnalyticsViewModel.kt
+++ app/src/main/java/app/simple/inure/viewmodels/panels/AnalyticsViewModel.kt
@@ -8,7 +8,7 @@ import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import app.simple.inure.apk.parsers.APKParser.getApkMeta
+import app.simple.inure.R
 import app.simple.inure.extension.viewmodels.WrappedViewModel
 import app.simple.inure.preferences.AnalyticsPreferences
 import app.simple.inure.util.SDKHelper
@@ -32,12 +32,22 @@ class AnalyticsViewModel(application: Application) : WrappedViewModel(applicatio
         }
     }
 
+    private val installLocationData: MutableLiveData<Pair<ArrayList<PieEntry>, ArrayList<Int>>> by lazy {
+        MutableLiveData<Pair<ArrayList<PieEntry>, ArrayList<Int>>>().also {
+            loadInstallLocationData()
+        }
+    }
+
     fun getMinimumOsData(): LiveData<Pair<ArrayList<PieEntry>, ArrayList<Int>>> {
         return minimumOsData
     }
 
     fun getTargetOsData(): LiveData<Pair<ArrayList<PieEntry>, ArrayList<Int>>> {
         return targetOsData
+    }
+
+    fun getInstallLocationData(): LiveData<Pair<ArrayList<PieEntry>, ArrayList<Int>>> {
+        return installLocationData
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
@@ -93,11 +103,32 @@ class AnalyticsViewModel(application: Application) : WrappedViewModel(applicatio
         }
     }
 
-    private fun PackageInfo.getMinSDK(): Int {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            applicationInfo.minSdkVersion
-        } else {
-            applicationInfo.getApkMeta().minSdkVersion.toInt()
+    private fun loadInstallLocationData() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val apps = packageManager.getInstalledPackages(PackageManager.GET_META_DATA)
+            val data = arrayListOf<PieEntry>()
+            val colors = arrayListOf<Int>()
+
+            var internal = 0F
+            var external = 0F
+            var auto = 0F
+            var unspecified = 0F
+
+            for (app in apps) {
+                when (app.installLocation) {
+                    PackageInfo.INSTALL_LOCATION_AUTO -> auto++
+                    PackageInfo.INSTALL_LOCATION_INTERNAL_ONLY -> internal++
+                    PackageInfo.INSTALL_LOCATION_PREFER_EXTERNAL -> external++
+                    -1 -> unspecified++
+                }
+            }
+
+            if (internal != 0F) data.add(PieEntry(internal, getString(R.string.internal)))
+            if (external != 0F) data.add(PieEntry(external, getString(R.string.prefer_external)))
+            if (auto != 0F) data.add(PieEntry(auto, getString(R.string.auto)))
+            if (unspecified != 0F) data.add(PieEntry(unspecified, getString(R.string.unspecified)))
+
+            installLocationData.postValue(Pair(data, colors))
         }
     }
 
