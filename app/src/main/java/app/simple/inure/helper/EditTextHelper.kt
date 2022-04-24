fe0e7791458f185d424404a16efc0403e4727df8
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-04-24 12:24:48 +0530

    notes_rev_06
      - Fixed bold, italics and underline in spans

--- app/src/main/java/app/simple/inure/helper/EditTextHelper.kt
+++ app/src/main/java/app/simple/inure/helper/EditTextHelper.kt
@@ -8,14 +8,48 @@ import android.widget.EditText
 
 object EditTextHelper {
     fun EditText.toBold() {
-        text.setSpan(StyleSpan(Typeface.BOLD), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        val spans: Array<StyleSpan> = text.getSpans(selectionStart, selectionEnd, StyleSpan::class.java)
+        var exists = false
+
+        for (span in spans) {
+            if (span.style == Typeface.BOLD) {
+                text.removeSpan(span)
+                exists = true
+            }
+        }
+
+        if (!exists) {
+            text.setSpan(StyleSpan(Typeface.BOLD), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        }
     }
 
     fun EditText.toItalics() {
-        text.setSpan(StyleSpan(Typeface.ITALIC), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        val spans: Array<StyleSpan> = text.getSpans(selectionStart, selectionEnd, StyleSpan::class.java)
+        var exists = false
+
+        for (span in spans) {
+            if (span.style == Typeface.ITALIC) {
+                text.removeSpan(span)
+                exists = true
+            }
+        }
+
+        if (!exists) {
+            text.setSpan(StyleSpan(Typeface.ITALIC), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        }
     }
 
     fun EditText.toUnderline() {
-        text.setSpan(UnderlineSpan(), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        val spans: Array<UnderlineSpan> = text.getSpans(selectionStart, selectionEnd, UnderlineSpan::class.java)
+        var exists = false
+
+        for (span in spans) {
+            text.removeSpan(span)
+            exists = true
+        }
+
+        if (!exists) {
+            text.setSpan(UnderlineSpan(), selectionStart, selectionEnd, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+        }
     }
 }
