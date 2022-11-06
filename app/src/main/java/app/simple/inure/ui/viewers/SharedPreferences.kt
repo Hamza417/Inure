6e1d918ede5a1ab973459afbb3af4398ff04cdea
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-11-06 20:59:51 +0530

    Build45
      - Added loading indicator in shared prefs panel.

--- app/src/main/java/app/simple/inure/ui/viewers/SharedPreferences.kt
+++ app/src/main/java/app/simple/inure/ui/viewers/SharedPreferences.kt
@@ -10,18 +10,22 @@ import app.simple.inure.R
 import app.simple.inure.adapters.details.AdapterResources
 import app.simple.inure.constants.BundleConstants
 import app.simple.inure.decorations.overscroll.CustomVerticalRecyclerView
+import app.simple.inure.decorations.views.CustomProgressBar
 import app.simple.inure.extensions.fragments.SearchBarScopedFragment
 import app.simple.inure.factories.panels.PackageInfoFactory
+import app.simple.inure.util.ViewUtils.gone
 import app.simple.inure.viewmodels.viewers.SharedPreferencesViewModel
 
 class SharedPreferences : SearchBarScopedFragment() {
 
+    private lateinit var loader: CustomProgressBar
     private lateinit var recyclerView: CustomVerticalRecyclerView
     private lateinit var sharedPreferencesViewModel: SharedPreferencesViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_shared_prefs, container, false)
 
+        loader = view.findViewById(R.id.loader)
         recyclerView = view.findViewById(R.id.shared_prefs_recycler_view)
 
         val packageInfoFactory = PackageInfoFactory(packageInfo)
@@ -35,6 +39,7 @@ class SharedPreferences : SearchBarScopedFragment() {
         startPostponedEnterTransition()
 
         sharedPreferencesViewModel.getSharedPrefs().observe(viewLifecycleOwner) {
+            loader.gone(animate = true)
             val adapterResources = AdapterResources(it, "")
 
             adapterResources.setOnResourceClickListener(object : AdapterResources.ResourceCallbacks {
@@ -52,6 +57,7 @@ class SharedPreferences : SearchBarScopedFragment() {
         }
 
         sharedPreferencesViewModel.getError().observe(viewLifecycleOwner) {
+            loader.gone(animate = true)
             showError(it)
         }
     }
