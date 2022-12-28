1529b2c160430966b75b835d25f31f802de58947
Hamza Rizwan <hamzarizwan243@gmail.com>
2022-12-28 20:56:00 +0530

    Added pretty licenses for OSL tags

--- app/src/main/java/app/simple/inure/extensions/viewmodels/PackageUtilsViewModel.kt
+++ app/src/main/java/app/simple/inure/extensions/viewmodels/PackageUtilsViewModel.kt
@@ -24,7 +24,7 @@ abstract class PackageUtilsViewModel(application: Application) : WrappedViewMode
 
     init {
         viewModelScope.launch(Dispatchers.IO) {
-            loadPackageData()
+            loadPackageData() // TODO - bug here
         }
     }
 
