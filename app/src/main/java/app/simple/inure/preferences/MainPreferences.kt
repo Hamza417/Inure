b243b7c1f5125f5e4436b94465ce92c6419a809f
Hamza417 <HamzaRizwan243@gmail.com>
2023-03-01 22:06:22 +0530

    Build163
      - #63 - Patched trial vulnerability

--- app/src/main/java/app/simple/inure/preferences/MainPreferences.kt
+++ app/src/main/java/app/simple/inure/preferences/MainPreferences.kt
@@ -141,8 +141,17 @@ object MainPreferences {
     // ---------------------------------------------------------------------------------------------------------- //
 
     fun getDaysLeft(): Int {
-        return (MAX_TRIAL_DAYS - CalendarUtils.getDaysBetweenTwoDates(Date(getFirstLaunchDate()), CalendarUtils.getToday()))
-            .coerceAtLeast(0)
+        return if (MAX_TRIAL_DAYS - CalendarUtils.getDaysBetweenTwoDates(Date(getFirstLaunchDate()), CalendarUtils.getToday()) <= MAX_TRIAL_DAYS) {
+            (MAX_TRIAL_DAYS - CalendarUtils.getDaysBetweenTwoDates(Date(getFirstLaunchDate()), CalendarUtils.getToday()))
+                .coerceAtLeast(0).coerceAtMost(15)
+        } else {
+            if (MAX_TRIAL_DAYS - CalendarUtils.getDaysBetweenTwoDates(Date(getFirstLaunchDate()), CalendarUtils.getToday()) > MAX_TRIAL_DAYS) {
+                0
+            } else {
+                MAX_TRIAL_DAYS - CalendarUtils.getDaysBetweenTwoDates(Date(getFirstLaunchDate()), CalendarUtils.getToday())
+                    .coerceAtLeast(0).coerceAtMost(15)
+            }
+        }
     }
 
     // ---------------------------------------------------------------------------------------------------------- //
