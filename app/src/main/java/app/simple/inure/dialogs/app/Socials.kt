484d7e3aa986b198f30033d12e6210098a96a906
Hamza417 <HamzaRizwan243@gmail.com>
2023-06-08 15:35:15 +0530

    Build78
      - Removed developer socials

--- app/src/main/java/app/simple/inure/dialogs/app/Socials.kt
+++ app/src/main/java/app/simple/inure/dialogs/app/Socials.kt
@@ -4,30 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.fragment.app.FragmentManager
 import app.simple.inure.R
 import app.simple.inure.decorations.ripple.DynamicRippleTextView
 import app.simple.inure.extensions.fragments.ScopedBottomSheetFragment
-import app.simple.inure.glide.util.ImageLoader.loadDrawable
 import app.simple.inure.util.IntentHelper.asUri
 import app.simple.inure.util.IntentHelper.openInBrowser
 
 class Socials : ScopedBottomSheetFragment() {
 
-    private lateinit var avatar: ImageView
-    private lateinit var twitter: DynamicRippleTextView
-    private lateinit var instagram: DynamicRippleTextView
     private lateinit var github: DynamicRippleTextView
+    private lateinit var playStore: DynamicRippleTextView
     private lateinit var close: DynamicRippleTextView
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.dialog_socials, container, false)
 
-        avatar = view.findViewById(R.id.avatar)
-        twitter = view.findViewById(R.id.twitter)
-        instagram = view.findViewById(R.id.instagram)
         github = view.findViewById(R.id.github)
+        playStore = view.findViewById(R.id.play_store)
         close = view.findViewById(R.id.close)
 
         return view
@@ -36,14 +30,8 @@ class Socials : ScopedBottomSheetFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        avatar.loadDrawable(R.drawable.avatar)
-
-        twitter.setOnClickListener {
-            getString(R.string.twitter_profile_link).asUri().openInBrowser(requireContext())
-        }
-
-        instagram.setOnClickListener {
-            getString(R.string.instagram_profile_link).asUri().openInBrowser(requireContext())
+        playStore.setOnClickListener {
+            getString(R.string.play_profile_link).asUri().openInBrowser(requireContext())
         }
 
         github.setOnClickListener {
