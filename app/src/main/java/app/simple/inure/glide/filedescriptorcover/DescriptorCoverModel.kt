2cafc83be519292268c6be75144dbcf385cdae41
Hamza417 <HamzaRizwan243@gmail.com>
2023-09-21 21:55:51 +0530

    Build94
      - Optimized image loader framework
    
      // skip build apk

--- app/src/main/java/app/simple/inure/glide/filedescriptorcover/DescriptorCoverModel.kt
+++ app/src/main/java/app/simple/inure/glide/filedescriptorcover/DescriptorCoverModel.kt
@@ -3,7 +3,20 @@ package app.simple.inure.glide.filedescriptorcover
 import android.content.Context
 import android.net.Uri
 
-class DescriptorCoverModel(
-        val context: Context,
-        val fileUri: Uri
-)
+class DescriptorCoverModel(val context: Context, val fileUri: Uri) {
+
+    override fun hashCode(): Int {
+        return fileUri.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is DescriptorCoverModel) {
+            return fileUri == other.fileUri
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return "DescriptorCoverModel(context=$context, fileUri=$fileUri)"
+    }
+}
--- app/src/main/java/app/simple/inure/glide/uricover/UriCoverModel.kt
+++ app/src/main/java/app/simple/inure/glide/uricover/UriCoverModel.kt
@@ -3,4 +3,19 @@ package app.simple.inure.glide.uricover
 import android.content.Context
 import android.net.Uri
 
-class UriCoverModel(val context: Context, val artUri: Uri)
+class UriCoverModel(val context: Context, val artUri: Uri) {
+    override fun hashCode(): Int {
+        return artUri.hashCode()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other is UriCoverModel) {
+            return artUri == other.artUri
+        }
+        return false
+    }
+
+    override fun toString(): String {
+        return "UriCoverModel(context=$context, artUri=$artUri)"
+    }
+}
