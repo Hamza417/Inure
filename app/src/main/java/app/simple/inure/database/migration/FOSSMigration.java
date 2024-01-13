92327ca4df37e4e166a7347562593eec461d05f2
Hamza417 <HamzaRizwan243@gmail.com>
2024-01-14 00:03:15 +0530

    Build99
      - Fixed FOSS migrations crashing below Android 10

--- app/src/main/java/app/simple/inure/database/migration/FOSSMigration.java
+++ app/src/main/java/app/simple/inure/database/migration/FOSSMigration.java
@@ -1,5 +1,7 @@
 package app.simple.inure.database.migration;
 
+import android.database.sqlite.SQLiteException;
+
 import androidx.annotation.NonNull;
 import androidx.room.migration.Migration;
 import androidx.sqlite.db.SupportSQLiteDatabase;
@@ -12,12 +14,30 @@ public class FOSSMigration extends Migration {
     
     @Override
     public void migrate(@NonNull SupportSQLiteDatabase supportSQLiteDatabase) {
-        /*
-         * I've renamed version_code to license
-         */
-        supportSQLiteDatabase.execSQL("ALTER TABLE foss RENAME COLUMN version_code TO license");
-        
-        // Also empty all the license entries
-        supportSQLiteDatabase.execSQL("UPDATE foss SET license = ''");
+        try {
+            /*
+             * I've renamed version_code to license
+             */
+            supportSQLiteDatabase.execSQL("ALTER TABLE foss RENAME COLUMN version_code TO license");
+            
+            // Also empty all the license entries
+            supportSQLiteDatabase.execSQL("UPDATE foss SET license = ''");
+        } catch (SQLiteException e) {
+            try {
+                // Device is below Android 10 and doesn't support RENAME COLUMN
+                // Create a new table with the new schema
+                supportSQLiteDatabase.execSQL("CREATE TABLE foss_new (package_name TEXT NOT NULL, license TEXT NOT NULL, is_foss INTEGER NOT NULL, PRIMARY KEY(package_name))");
+                supportSQLiteDatabase.execSQL("INSERT INTO foss_new (package_name, license, is_foss) SELECT package_name, '', is_foss FROM foss");
+                supportSQLiteDatabase.execSQL("DROP TABLE foss");
+                supportSQLiteDatabase.execSQL("ALTER TABLE foss_new RENAME TO foss");
+            } catch (SQLiteException ignored) {
+                try {
+                    // It crashed again, so we'll just drop the table and recreate it
+                    supportSQLiteDatabase.execSQL("DROP TABLE foss");
+                    supportSQLiteDatabase.execSQL("CREATE TABLE foss (package_name TEXT NOT NULL, license TEXT NOT NULL, is_foss INTEGER NOT NULL, PRIMARY KEY(package_name))");
+                } catch (SQLiteException ignored1) {
+                }
+            }
+        }
     }
 }
