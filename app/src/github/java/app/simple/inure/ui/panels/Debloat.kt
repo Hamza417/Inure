f792a6d7c7b1957f569dc4d3371a0adc69da7915
Hamza417 <HamzaRizwan243@gmail.com>
2024-01-22 22:05:59 +0530

    Build99.1.0
      - Added basic classes for Debloat

---
+++ app/src/github/java/app/simple/inure/ui/panels/Debloat.kt
@@ -0,0 +1,5 @@
+package app.simple.inure.ui.panels
+
+import app.simple.inure.extensions.fragments.ScopedFragment
+
+class Debloat : ScopedFragment()
---
+++ app/src/github/java/app/simple/inure/viewmodels/panels/DebloatViewModel.kt
@@ -0,0 +1,6 @@
+package app.simple.inure.viewmodels.panels
+
+import android.app.Application
+import app.simple.inure.extensions.viewmodels.PackageUtilsViewModel
+
+class DebloatViewModel(application: Application) : PackageUtilsViewModel(application)
