b4a3a640cc239708a773c85192015693f2b22d77
Hamza417 <HamzaRizwan243@gmail.com>
2024-04-05 03:26:46 +0530

    fix: no close button in Music

--- app/src/main/java/app/simple/inure/services/AudioService.kt
+++ app/src/main/java/app/simple/inure/services/AudioService.kt
@@ -347,7 +347,7 @@ class AudioService : Service(),
             stopForeground(true)
         }
 
-        IntentHelper.sendLocalBroadcastIntent(ServiceConstants.actionQuitMusicServicePager, applicationContext)
+        IntentHelper.sendLocalBroadcastIntent(ServiceConstants.actionQuitMusicService, applicationContext)
         stopSelf()
     }
 
