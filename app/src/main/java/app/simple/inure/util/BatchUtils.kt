db9bbaa26dafb9d4ec442cca28aa78c92c9fa402
Hamza417 <HamzaRizwan243@gmail.com>
2024-09-29 17:25:38 +0530

    Fix batch extract crashing on special characters

--- app/src/main/java/app/simple/inure/util/BatchUtils.kt
+++ app/src/main/java/app/simple/inure/util/BatchUtils.kt
@@ -6,22 +6,25 @@ import app.simple.inure.apk.utils.PackageData
 import app.simple.inure.constants.Misc
 
 object BatchUtils {
-
     fun Context.getBundlePathAndFileName(packageInfo: PackageInfo): String {
         val stringBuilder = StringBuilder()
         stringBuilder.append(PackageData.getPackageDir(this))
         stringBuilder.append("/")
-        stringBuilder.append(packageInfo.applicationInfo.name)
-        stringBuilder.append("_(${packageInfo.versionName})")
+        stringBuilder.append(packageInfo.applicationInfo.name.sanitize())
+        stringBuilder.append("_(${packageInfo.versionName.sanitize()})")
         stringBuilder.append(Misc.splitApkFormat)
         return stringBuilder.toString()
     }
 
     fun getApkPathAndFileName(packageInfo: PackageInfo): String {
         val stringBuilder = StringBuilder()
-        stringBuilder.append(packageInfo.applicationInfo.name)
-        stringBuilder.append("_(${packageInfo.versionName})")
+        stringBuilder.append(packageInfo.applicationInfo.name.sanitize())
+        stringBuilder.append("_(${packageInfo.versionName.sanitize()})")
         stringBuilder.append(".apk")
         return stringBuilder.toString()
+    }
+
+    private fun String.sanitize(): String {
+        return replace(Regex("[^a-zA-Z0-9._-]"), "_")
     }
 }
