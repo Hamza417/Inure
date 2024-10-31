79c5cede66c7ba351ab6c0bd9429a146f3e97b0f
Hamza417 <HamzaRizwan243@gmail.com>
2024-10-31 23:31:56 +0530

    Add archive icons

--- app/src/main/java/app/simple/inure/extensions/activities/BaseActivity.kt
+++ app/src/main/java/app/simple/inure/extensions/activities/BaseActivity.kt
@@ -94,7 +94,7 @@ open class BaseActivity : AppCompatActivity(),
     private var loader: Loader? = null
 
     private var cutoutDepth = 0
-    private var requestCode = 0x2756
+    private var requestCode = 0x2774
 
     override fun attachBaseContext(newBaseContext: Context) {
         SharedPreferences.init(newBaseContext)
