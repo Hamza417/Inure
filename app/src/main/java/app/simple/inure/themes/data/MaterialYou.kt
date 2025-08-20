7b0ae6922c42c7c4a8e9927a9b9a2d960f94851c
Hamza417 <23103729+Hamza417@users.noreply.github.com>
2025-08-20 10:22:48 +0530

    fix: wrong shade for highlight in material you theme

--- app/src/main/java/app/simple/inure/themes/data/MaterialYou.kt
+++ app/src/main/java/app/simple/inure/themes/data/MaterialYou.kt
@@ -55,7 +55,7 @@ object MaterialYou {
 
         background = ContextCompat.getColor(this, android.R.color.system_neutral1_50)
         viewerBackground = ContextCompat.getColor(this, android.R.color.system_neutral1_100)
-        highlightBackground = ContextCompat.getColor(this, android.R.color.system_accent2_100)
+        highlightBackground = ContextCompat.getColor(this, android.R.color.system_neutral1_100)
         selectedBackground = ContextCompat.getColor(this, android.R.color.system_accent2_100)
         dividerBackground = ContextCompat.getColor(this, android.R.color.system_accent3_300)
 
@@ -75,7 +75,7 @@ object MaterialYou {
 
         backgroundDark = ContextCompat.getColor(this, android.R.color.system_neutral1_900)
         viewerBackgroundDark = ContextCompat.getColor(this, android.R.color.system_neutral1_800)
-        highlightBackgroundDark = ContextCompat.getColor(this, android.R.color.system_accent1_700)
+        highlightBackgroundDark = ContextCompat.getColor(this, android.R.color.system_neutral1_800)
         selectedBackgroundDark = ContextCompat.getColor(this, android.R.color.system_accent2_600)
         dividerBackgroundDark = ContextCompat.getColor(this, android.R.color.system_accent3_200)
 
