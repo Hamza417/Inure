df85b8b6c34795508b0614a8cc0f214ef5149ee8
Hamza Rizwan <hamzarizwan243@gmail.com>
2026-01-06 12:33:34 +0530

    change: use pm clear for clearing app cache

--- app/src/main/java/app/simple/inure/viewmodels/dialogs/ClearCacheViewModel.kt
+++ app/src/main/java/app/simple/inure/viewmodels/dialogs/ClearCacheViewModel.kt
@@ -1,26 +1,18 @@
 package app.simple.inure.viewmodels.dialogs
 
 import android.app.Application
-import android.content.Context
-import android.content.pm.IPackageDataObserver
 import android.content.pm.PackageInfo
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import app.simple.inure.apk.utils.PackageUtils.isSystemApp
-import app.simple.inure.constants.Misc
 import app.simple.inure.constants.Warnings
 import app.simple.inure.exceptions.InureShellException
 import app.simple.inure.extensions.viewmodels.RootShizukuViewModel
 import app.simple.inure.helpers.ShizukuServiceHelper
-import app.simple.inure.shizuku.ShizukuUtils
 import com.topjohnwu.superuser.Shell
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.io.File
-import java.util.Collections
 
 class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo) : RootShizukuViewModel(application) {
     private val result: MutableLiveData<String> by lazy {
@@ -29,11 +21,7 @@ class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo
 
     private val success: MutableLiveData<String> by lazy {
         MutableLiveData<String>().also {
-            if (applicationContext().applicationInfo.isSystemApp()) {
-                applicationContext().clearCache()
-            } else {
-                initializeCoreFramework()
-            }
+            initializeCoreFramework()
         }
     }
 
@@ -47,8 +35,6 @@ class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo
 
     private fun runCommand() {
         viewModelScope.launch(Dispatchers.IO) {
-            delay(Misc.delay)
-
             kotlin.runCatching {
                 Shell.cmd(getCommand()).submit { shellResult ->
                     kotlin.runCatching {
@@ -86,10 +72,33 @@ class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo
         }
     }
 
-    private fun runShizuku() {
+    private fun runShizuku(shizukuServiceHelper: ShizukuServiceHelper) {
         viewModelScope.launch(Dispatchers.IO) {
             kotlin.runCatching {
-                ShizukuUtils.clearAppCache(setOf(packageInfo.packageName))
+                shizukuServiceHelper.service?.simpleExecute(getCommand()).let { shellResult ->
+                    kotlin.runCatching {
+                        for (i in shellResult?.output!!.lines()) {
+                            result.postValue("\n" + i)
+                            if (i.contains("Exception") || i.contains("not exist")) {
+                                throw InureShellException("Execution Failed...")
+                            }
+                        }
+                    }.onSuccess {
+                        if (shellResult?.isSuccess == true) {
+                            success.postValue("Done")
+                        } else {
+                            success.postValue("Failed")
+                        }
+                    }.getOrElse {
+                        it.printStackTrace()
+                        result.postValue("\n" + it.message!!)
+                        if (shellResult?.isSuccess == true) {
+                            success.postValue("Done")
+                        } else {
+                            success.postValue("Failed")
+                        }
+                    }
+                }
             }.onSuccess {
                 Log.d("ClearCacheViewModel", "Cache cleared: ${packageInfo.packageName}")
                 success.postValue("Done")
@@ -106,17 +115,7 @@ class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo
     }
 
     private fun getCommand(): String {
-        val packageContext: Context = applicationContext().createPackageContext(packageInfo.packageName, 0)
-        val directories: MutableList<File?> = ArrayList()
-        directories.add(packageContext.cacheDir)
-        Collections.addAll(directories, *packageContext.externalCacheDirs)
-        val command = StringBuilder("rm -rf")
-        for (directory in directories) {
-            Log.d("ClearCacheViewModel", "getCommand: ${directory?.absolutePath}")
-            command.append(" \"" + directory?.absolutePath.toString() + "\"")
-        }
-
-        return command.toString()
+        return "pm clear --cache-only --user ${getCurrentUser()} ${packageInfo.packageName}"
     }
 
     override fun onShellCreated(shell: Shell?) {
@@ -129,23 +128,6 @@ class ClearCacheViewModel(application: Application, val packageInfo: PackageInfo
     }
 
     override fun onShizukuCreated(shizukuServiceHelper: ShizukuServiceHelper) {
-        runShizuku()
-    }
-
-    /**
-     * Clear cache of installed app using Android APIs
-     */
-    private fun Context.clearCache() {
-        val pm = packageManager
-        val method = pm.javaClass.getMethod("freeStorageAndNotify", Long::class.javaPrimitiveType, IPackageDataObserver::class.java)
-        method.invoke(pm, Long.MAX_VALUE, object : IPackageDataObserver.Stub() {
-            override fun onRemoveCompleted(packageName: String, succeeded: Boolean) {
-                if (succeeded) {
-                    success.postValue("Done")
-                } else {
-                    success.postValue("Failed")
-                }
-            }
-        })
+        runShizuku(shizukuServiceHelper)
     }
 }
